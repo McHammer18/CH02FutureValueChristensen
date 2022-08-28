d730e2c707a76c9e6124d45c16ef2933dd2a5afb
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-08-28 17:29:57 -0500

    Add project files.

---
+++ CH02FutureValueChristensen/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CH02FutureValueChristensen.Models;
+
+namespace CH02FutureValueChristensen.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model)
+        {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
