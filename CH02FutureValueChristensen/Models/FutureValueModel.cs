c8140ae9e57298b4b4bd43501f127eb1cc8f4c14
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-08-29 10:49:14 -0500

    user validation

--- CH02FutureValueChristensen/Models/FutureValueModel.cs
+++ CH02FutureValueChristensen/Models/FutureValueModel.cs
@@ -1,21 +1,27 @@
-﻿namespace CH02FutureValueChristensen.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace CH02FutureValueChristensen.Models
 {
     public class FutureValueModel
     {
-        public decimal MonthlyInvestment { get; set; }
+        [Required(ErrorMessage = "Please Enter a Monthly Investment.")]
+        [Range(1,500, ErrorMessage = "Investment must be between 1 and 500.")]
+        public decimal? MonthlyInvestment { get; set; }
 
-        public decimal YearlyInterestRate { get; set; }
-
+        [Required(ErrorMessage = "Please enter a Yearly InterestRate.")]
+        [Range(0.1, 10.0, ErrorMessage ="Interesst Rate must be between 0.1 and 10.0.")]
+        public decimal? YearlyInterestRate { get; set; }
+        [Required(ErrorMessage = " Please enter the number of years you would like to have the invesstment.")]
+        [Range(1, 50, ErrorMessage = "Investment can only be in for 1 to 50 years.")]
         public int Years  { get; set; }
 
         public decimal CalculateFutureValue()
         {
             int months = Years * 12;
-            decimal monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            decimal monthlyInterestRate = YearlyInterestRate.Value / 12 / 100;
             decimal futureValue = 0;
             for (int i = 0; i < months; i++)
             {
-                futureValue = (futureValue + MonthlyInvestment) *
+                futureValue = (futureValue + MonthlyInvestment.Value) *
                     (1 + monthlyInterestRate);
             }
             return futureValue;
